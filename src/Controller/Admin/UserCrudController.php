6f9b0f81e78b10041fc0b23587cf6220ec0dc92a
sophiedannery <dannery.sophie@gmail.com>
2025-03-05 11:52:27 +0100

    ajout crud user

---
+++ src/Controller/Admin/UserCrudController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class UserCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return User::class;
+    }
+
+
+    public function configureFields(string $pageName): iterable
+    {
+        yield IdField::new('id')->hideOnForm();
+        yield TextField::new('pseudo', 'Pseudo');
+        yield EmailField::new('email', 'Email');
+        yield TextField::new('password', 'Mot de passe')->onlyOnForms();
+        yield ChoiceField::new('type', 'Statut')
+            ->setChoices([
+                'Chauffeur' => 'chauffeur',
+                'Passager' => 'passager',
+                'Passager et Chauffeur' => 'passager_chauffeur'
+            ])
+            ->renderExpanded();
+        yield IntegerField::new('credit', 'Crédits')->hideOnForm();
+        yield ChoiceField::new('roles', 'Rôles')
+            ->setChoices([
+                'Admin' => 'ROLE_ADMIN',
+                'User' => 'ROLE_USER'
+            ])
+            ->allowMultipleChoices()
+            ->hideOnForm();
+    }
+}
